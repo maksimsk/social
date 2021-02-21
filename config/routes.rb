6d75b678032c64d05301dd598aa48c03e7bd94c6
Maksims Kuznecovs <maksims.kuznecovs@latori.com>
2021-02-21 13:49:35 +0100

    routes for posts, publics, comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'public/index'
+  resources :posts
+  resources :publics
+  resources :comments
+  
   
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
