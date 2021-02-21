6141be4e21b0b60a1a7705fae56faf83bdc092cc
Maksims Kuznecovs <maksims.kuznecovs@latori.com>
2021-02-21 13:47:39 +0100

    created "publics" controller instead of "public"

---
+++ app/controllers/publics_controller.rb
@@ -0,0 +1,64 @@
+class PublicsController < ApplicationController
+  before_action :set_public, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @publics = Public.all
+  end
+
+
+  def show
+  end
+
+  def new
+    @public = Public.new
+  end
+
+  def edit
+  end
+
+  def create
+    @public = Public.new(public_params)
+
+    respond_to do |format|
+      if @public.save
+        format.html { redirect_to @public, notice: 'Public was successfully created.' }
+        format.json { render :show, status: :created, location: @public }
+      else
+        format.html { render :new }
+        format.json { render json: @public.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @public.update(public_params)
+        format.html { redirect_to @public, notice: 'Public was successfully updated.' }
+        format.json { render :show, status: :ok, location: @public }
+      else
+        format.html { render :edit }
+        format.json { render json: @public.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @public.destroy
+    respond_to do |format|
+      format.html { redirect_to publics_url, notice: 'Public was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_public
+      @public = Public.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def public_params
+      params.fetch(:public, {})
+    end
+end
--- db/migrate/20210122110322_create_publics.rb
+++ db/migrate/20210122110322_create_publics.rb
@@ -1,7 +1,7 @@
 class CreatePublics < ActiveRecord::Migration[6.0]
   def change
     create_table :publics do |t|
-      t.string :name
+      t.string :name, null: false,
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
