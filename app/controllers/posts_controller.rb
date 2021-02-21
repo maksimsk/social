15777a1cecf0e8fa808c132443d4c3f83fe36e55
Maksims Kuznecovs <maksims.kuznecovs@latori.com>
2021-02-21 18:22:57 +0100

    update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.fetch(:post, {})
+      params.fetch(:post, {}).permit(:title, :text, :image, :public_id, :user_id)
     end
 end
