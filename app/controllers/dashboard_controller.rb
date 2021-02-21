a10e123de129a8762e157ed3b6c78ab2f09270bd
Maksims Kuznecovs <maksims.kuznecovs@latori.com>
2021-02-21 17:01:31 +0100

    add devise controllers

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class DashboardController < ApplicationController
+
+  before_action :authenticate_user!
   def index
+    @user = current_user
+    @user_publics = PublicUser.where(user: @user)
+    p @user_publics
   end
 end
