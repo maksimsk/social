d97de3137ffcd1d9a276f1e76ba0dc7303a2e298
Maksims Kuznecovs <maksims.kuznecovs@latori.com>
2021-02-22 22:25:43 +0100

    set up dislikes controller

---
+++ app/controllers/dislikes_controller.rb
@@ -0,0 +1,23 @@
+class DislikesController < ApplicationController
+  before_action :find_post
+
+  def create
+    unless Like.find_by(post_id: @post.id, user_id: current_user.id).blank?
+      Like.find_by(post_id: @post.id, user_id: current_user.id).delete
+    end
+    
+    if Dislike.find_by(post_id: @post.id, user_id: current_user.id).blank?
+      @post.dislikes.create(user_id: current_user.id)
+      redirect_to root_path, notice: 'You Disliked the Post'
+    else
+      redirect_to root_path, notice: 'You have already disliked this post'
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+    p params
+  end
+end
