4d82ffff91e690283910e59890204500eb9e3775
Maksims Kuznecovs <maksims.kuznecovs@latori.com>
2021-02-22 22:26:28 +0100

    set up dashboard controller and views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class DashboardController < ApplicationController
-
   before_action :authenticate_user!
+
   def index
     @user = current_user
     @user_publics = PublicUser.where(user: @user)
-    p @user_publics
+    @user_likes = Like.where(user_id: @user.id)
   end
 end
